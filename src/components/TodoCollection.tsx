c30c084c0739273a28496f2bcb0c49bf6b323eeb
Epikle <98042337+Epikle@users.noreply.github.com>
2022-11-22 15:03:22 +0200

    Added item animation

--- src/components/TodoCollection.tsx
+++ src/components/TodoCollection.tsx
@@ -1,16 +1,17 @@
-import { FC, CSSProperties } from 'react';
+import { FC, CSSProperties, useEffect, useRef } from 'react';
+import autoAnimate from '@formkit/auto-animate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
 
 import TodoItem from './TodoItem';
 import { useAppDispatch, useAppSelector } from '../hooks/useRedux';
 import { TCollection } from '../types';
-
-import styles from './TodoCollection.module.scss';
 import {
   resetSelection,
   setSelectedCollection,
 } from '../context/selectedSlice';
+
+import styles from './TodoCollection.module.scss';
 
 type Props = {
   collection: TCollection;
@@ -20,6 +21,7 @@ const TodoCollection: FC<Props> = ({ collection }) => {
   const dispatch = useAppDispatch();
   const selectedCollection = useAppSelector((state) => state.selected);
   const isSelected = selectedCollection.id === collection.id;
+  const parent = useRef<HTMLUListElement>(null);
 
   const listStyles: CSSProperties = {
     borderColor: collection.color,
@@ -39,6 +41,10 @@ const TodoCollection: FC<Props> = ({ collection }) => {
     dispatch(setSelectedCollection({ id, title, color }));
   };
 
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
+
   const doneTodos = collection.todos.filter((todo) => todo.done).length;
   const totalTodos = collection.todos.length;
   const showDone = totalTodos > 0 ? `${doneTodos}/${totalTodos}` : '';
@@ -54,7 +60,7 @@ const TodoCollection: FC<Props> = ({ collection }) => {
           {collection.title}
         </button>
       </h2>
-      <ul className={styles['item-list']}>
+      <ul className={styles['item-list']} ref={parent}>
         {collection.todos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))}
