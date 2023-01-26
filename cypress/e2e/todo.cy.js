2f039d3d2af49c1e268cb730e51b601ccb6d89cd
Epikle <98042337+Epikle@users.noreply.github.com>
2023-01-26 20:16:25 +0200

    Update todo.cy.js

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -7,22 +7,26 @@ describe('oktaniTODO', () => {
 
   it('displays collection named First Collection default', () => {
     cy.get('article').first().should('have.text', 'First Collection').click();
+    cy.wait(500);
   });
 
   it('selects first collection and deselects it', () => {
     cy.get('article').first().should('have.text', 'First Collection').click();
+    cy.wait(500);
     cy.get('form > button').click().should('not.be.visible');
+    cy.wait(500);
     cy.get('input[placeholder="Add a new collection"]').should('be.visible');
+    cy.wait(500);
   });
 
   it('selects collection and adds item to it', () => {
     cy.get('article').first().should('have.text', 'First Collection').click();
-
+    cy.wait(500);
     cy.get('input[placeholder="Add a new todo to First Collection"]').type(
       'Test todo item',
     );
     cy.get('button[aria-label="Add"]').click();
-
+    cy.wait(500);
     cy.get('article')
       .first()
       .children()
