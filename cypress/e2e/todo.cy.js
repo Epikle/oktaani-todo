0cea5dffe692807d5de4d0837d1abddeb0d0757c
Epikle <98042337+Epikle@users.noreply.github.com>
2023-01-31 21:33:53 +0200

    Update todo.cy.js

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -6,12 +6,18 @@ describe('oktaniTODO', () => {
   });
 
   it('displays collection named First Collection default', () => {
-    cy.get('article').first().should('have.text', 'First Collection').click();
+    cy.get('article')
+      .first()
+      .should('have.text', '✨ First Collection ✨')
+      .click();
     cy.wait(500);
   });
 
   it('selects first collection and deselects it', () => {
-    cy.get('article').first().should('have.text', 'First Collection').click();
+    cy.get('article')
+      .first()
+      .should('have.text', '✨ First Collection ✨')
+      .click();
     cy.wait(500);
     cy.get('form > button').click().should('not.be.visible');
     cy.wait(500);
@@ -20,11 +26,14 @@ describe('oktaniTODO', () => {
   });
 
   it('selects collection and adds item to it', () => {
-    cy.get('article').first().should('have.text', 'First Collection').click();
+    cy.get('article')
+      .first()
+      .should('have.text', '✨ First Collection ✨')
+      .click();
     cy.wait(500);
-    cy.get('input[placeholder="Add a new todo to First Collection"]').type(
-      'Test todo item',
-    );
+    cy.get(
+      'input[placeholder="Add a new todo to ✨ First Collection ✨"]',
+    ).type('Test todo item');
     cy.get('button[aria-label="Add"]').click();
     cy.wait(500);
     cy.get('article')
