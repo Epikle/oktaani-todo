d58f39cf6f5c064f226ac15bdaad3a5f5aad6ea3
Epikle <98042337+Epikle@users.noreply.github.com>
2023-02-01 14:18:37 +0200

    Refactoring

---
+++ src/components/TodoForm/ColorChooser.tsx
@@ -0,0 +1,65 @@
+import { Dispatch, FC, SetStateAction, useEffect, useRef } from 'react';
+
+import { editCollection } from '../../context/todoSlice';
+import { useAppDispatch } from '../../hooks/useRedux';
+import type { Texts, TSelected } from '../../types';
+
+import styles from './ColorChooser.module.scss';
+
+type Props = {
+  defaultColor: string;
+  color: string;
+  setColor: Dispatch<SetStateAction<string>>;
+  selectedCollection: TSelected;
+  text: Texts;
+};
+
+const ColorChooser: FC<Props> = ({
+  defaultColor,
+  color,
+  setColor,
+  selectedCollection,
+  text,
+}) => {
+  const colorInputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+
+  const colorInputHandler = () => {
+    if (!colorInputRef.current) return;
+    if (!selectedCollection.color) {
+      setColor(colorInputRef.current.value);
+      return;
+    }
+
+    const editedCollection = {
+      id: selectedCollection.id,
+      title: selectedCollection.title,
+      color: colorInputRef.current.value,
+    };
+
+    dispatch(editCollection(editedCollection));
+  };
+
+  useEffect(() => {
+    if (!colorInputRef.current) return;
+    if (!selectedCollection.color) {
+      colorInputRef.current.value = color;
+      return;
+    }
+
+    colorInputRef.current.value = selectedCollection.color;
+  }, [selectedCollection.color]);
+
+  return (
+    <input
+      type="color"
+      title={text.header.setColorTitle}
+      className={styles['color-picker']}
+      ref={colorInputRef}
+      defaultValue={defaultColor}
+      onBlur={colorInputHandler}
+    />
+  );
+};
+
+export default ColorChooser;
