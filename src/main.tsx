f19a491dbfcdf7489f108b788322cc114f64f01f
Epikle <98042337+Epikle@users.noreply.github.com>
2023-02-05 11:47:23 +0200

    React DnD multi backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd-multi-backend';
+import { HTML5toTouch } from 'rdndmb-html5-to-touch';
 
 import { setupStore } from './context/store';
 import App from './App';
@@ -12,7 +12,7 @@ import './main.scss';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={setupStore()}>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider options={HTML5toTouch}>
         <App />
       </DndProvider>
     </Provider>
