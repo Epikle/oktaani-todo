18889ac8201124d2f691f09fb06cf986aef63eab
Epikle <98042337+Epikle@users.noreply.github.com>
2023-04-07 18:38:16 +0300

    :bug: Tests

--- src/components/UI/Button.test.tsx
+++ src/components/UI/Button.test.tsx
@@ -8,18 +8,17 @@ import Button from './Button';
 describe('Button', () => {
   const btnSetup = {
     testId: 'btn-test',
-    content: 'Test button',
     title: 'Btn title',
     className: 'test-class',
   };
 
   it('Button is showing with all attributes', () => {
-    render(<Button {...btnSetup} />);
+    render(<Button {...btnSetup}>Test button</Button>);
 
     const btn = screen.getByTestId(btnSetup.testId);
 
     expect(btn).toBeInTheDocument();
-    expect(btn).toHaveTextContent(btnSetup.content);
+    expect(btn).toHaveTextContent('Test button');
     expect(btn).toHaveAccessibleDescription(btnSetup.title);
     expect(btn).toHaveAttribute('title', btnSetup.title);
     expect(btn).toHaveClass(btnSetup.className);
@@ -28,7 +27,11 @@ describe('Button', () => {
   it('When clicked should call handlerFn', () => {
     const onClick = vi.fn();
 
-    render(<Button onClick={onClick} {...btnSetup} />);
+    render(
+      <Button onClick={onClick} {...btnSetup}>
+        Test button
+      </Button>,
+    );
     const btn = screen.getByTestId(btnSetup.testId);
     btn.click();
 
