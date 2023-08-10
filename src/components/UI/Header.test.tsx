97871883bd7747a679092fd6c81e81b112b920e6
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-10 20:31:56 +0300

    :rotating_light: Test Header

--- src/components/UI/Header.test.tsx
+++ src/components/UI/Header.test.tsx
@@ -1,12 +1,66 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Header from './Header';
+import useSelectedStore from '../../context/useSelectedStore';
+import useTodoStore from '../../context/useTodoStore';
+
+vi.mock('@formkit/auto-animate');
+const spySelected = vi.spyOn(useSelectedStore.getState(), 'selected', 'get');
+const spyDeleteCol = vi.spyOn(useTodoStore.getState().actions, 'deleteCollection').mockImplementation(async () => {});
 
 describe('Header', () => {
-  it('Logo is showing', () => {
-    render(<Header />);
-
-    expect(screen.getByText('oktaani')).toBeInTheDocument();
+  it('Should show logo', () => {
+    const { getByText } = render(<Header />);
+    expect(getByText('oktaani')).toBeInTheDocument();
   });
+
+  it('Should show settings button', () => {
+    const { getByTestId } = render(<Header />);
+    const settingsBtnElem = getByTestId('btn-settings');
+    expect(settingsBtnElem).toBeInTheDocument();
+  });
+
+  it('Should show settings button', () => {
+    const { getByTestId, queryByTestId } = render(<Header />);
+    const settingsBtnElem = getByTestId('btn-settings');
+    const todoControlsElem = queryByTestId('todo-controls');
+    expect(settingsBtnElem).toBeInTheDocument();
+    expect(todoControlsElem).not.toBeInTheDocument();
+  });
+
+  it('Should show todo controls', () => {
+    spySelected.mockReturnValue(true);
+    const { queryByTestId, getByTestId } = render(<Header />);
+    const todoControlsElem = getByTestId('todo-controls');
+    const settingsBtnElem = queryByTestId('btn-settings');
+    expect(todoControlsElem).toBeInTheDocument();
+    expect(settingsBtnElem).not.toBeInTheDocument();
+  });
+
+  it('Should show delete confirm and cancel it', () => {
+    spySelected.mockReturnValue(true);
+    const { getByTestId } = render(<Header />);
+    const deleteBtnElem = getByTestId('delete-collection-btn');
+    fireEvent.click(deleteBtnElem);
+    const cancelBtnElem = getByTestId('cancel-delete-btn');
+    fireEvent.click(cancelBtnElem);
+    expect(spyDeleteCol).toBeCalledTimes(0);
+  });
+
+  it('Should show delete confirm and accept it', async () => {
+    spySelected.mockReturnValue(true);
+    const { getByTestId } = render(<Header />);
+    const deleteBtnElem = getByTestId('delete-collection-btn');
+    fireEvent.click(deleteBtnElem);
+    const confirmBtnElem = getByTestId('confirm-delete-btn');
+    fireEvent.click(confirmBtnElem);
+    await waitFor(() => {
+      expect(spyDeleteCol).toBeCalledTimes(1);
+    });
+  });
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
 });
