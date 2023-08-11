29c09029595d0ec97efd7e19d963841e74c918c6
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-11 13:23:27 +0300

    :recycle: Clean HelpSelection test

--- src/components/UI/Settings/HelpSelection.test.tsx
+++ src/components/UI/Settings/HelpSelection.test.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { fireEvent, render } from '@testing-library/react';
 
 import HelpSelection from './HelpSelection';
 import useTodoStore, { Collection } from '../../../context/useTodoStore';
@@ -29,47 +28,34 @@ const testCollection: Collection[] = [
 describe('HelpSelection', () => {
   it('Button should not have class help-active', () => {
     const { getByTestId } = render(<HelpSelection />);
-
     const btn = getByTestId('help-btn');
-
     expect(btn).toBeInTheDocument();
     expect(btn).not.toHaveClass('help-active');
   });
 
   it('Button should be disabled', () => {
     const { getByTestId } = render(<HelpSelection />);
-
     const btn = getByTestId('help-btn');
-
     expect(btn).toBeInTheDocument();
     expect(btn).toBeDisabled();
   });
 
   it('Button should not be disabled', () => {
-    vi.spyOn(useTodoStore.getState(), 'collections', 'get').mockImplementation(() => testCollection);
-
+    vi.spyOn(useTodoStore.getState(), 'collections', 'get').mockReturnValue(testCollection);
     const { getByTestId } = render(<HelpSelection />);
-
-    const btn = getByTestId('help-btn');
-
-    expect(btn).toBeInTheDocument();
-    expect(btn).not.toBeDisabled();
+    const btnElem = getByTestId('help-btn');
+    expect(btnElem).toBeInTheDocument();
+    expect(btnElem).not.toBeDisabled();
   });
 
   it('Button should have class help-active', () => {
-    vi.spyOn(useTodoStore.getState(), 'collections', 'get').mockImplementation(() => testCollection);
-
+    vi.spyOn(useTodoStore.getState(), 'collections', 'get').mockReturnValue(testCollection);
     const { getByTestId } = render(<HelpSelection />);
-
-    const btn = getByTestId('help-btn');
-
-    act(() => {
-      btn.click();
-    });
-
-    expect(btn).toBeInTheDocument();
-    expect(btn).not.toBeDisabled();
-    expect(btn.getAttribute('class')).toContain('help-active');
+    const btnElem = getByTestId('help-btn');
+    fireEvent.click(btnElem);
+    expect(btnElem).toBeInTheDocument();
+    expect(btnElem).not.toBeDisabled();
+    expect(btnElem.getAttribute('class')).toContain('help-active');
   });
 });
 
