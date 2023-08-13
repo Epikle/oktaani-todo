9c755f907808b6ea1a7df5b46cd90c16b445ec2d
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-13 17:40:10 +0300

    :bug: Fix e2e tests

--- cypress/e2e/shareTodo.cy.ts
+++ cypress/e2e/shareTodo.cy.ts
@@ -33,10 +33,10 @@ describe('Share todo', () => {
   });
 
   it('Should share created collection successfully and then cancel it', () => {
-    cy.intercept('GET', 'http://localhost:5000/api/v2/**', []);
-    cy.intercept('PUT', 'http://localhost:5000/api/v2/**', []);
-    cy.intercept('POST', 'http://localhost:5000/api/v2/**', []);
-    cy.intercept('DELETE', 'http://localhost:5000/api/v2/**', []);
+    cy.intercept('GET', '/api/v2/**', []);
+    cy.intercept('PUT', '/api/v2/**', []);
+    cy.intercept('POST', '/api/v2/**', []);
+    cy.intercept('DELETE', '/api/v2/**', []);
 
     cy.get('[data-testid="share-col-btn"]').click();
     cy.get('[data-testid="confirm-container"]').should('have.text', 'Share collection?');
--- cypress/e2e/todoLog.cy.ts
+++ cypress/e2e/todoLog.cy.ts
@@ -9,14 +9,15 @@ describe('Welcome page and settings', () => {
   });
 
   it('Should share collection and open log and close', () => {
-    cy.intercept('GET', 'http://localhost:5000/api/v2/**', []);
-    cy.intercept('PUT', 'http://localhost:5000/api/v2/**', []);
-    cy.intercept('POST', 'http://localhost:5000/api/v2/**', []);
+    cy.intercept('GET', '/api/v2/**', []).as('getLogs');
+    cy.intercept('PUT', '/api/v2/**', []);
+    cy.intercept('POST', '/api/v2/**', []);
 
     cy.get('[data-testid="share-col-btn"]').click();
     cy.get('[data-testid="confirm-container"]').should('have.text', 'Share collection?');
     cy.get('[data-testid="confirm-delete-btn"]').click();
     cy.get('[aria-label="Show Changelog"]').click();
+    cy.wait('@getLogs');
     cy.get('[data-testid="log-container"]').contains('Changelog');
     cy.get('[aria-label="Show Changelog"]').click();
     cy.get('[data-testid="log-container"]').should('not.exist');
