59c789bba30db193d89459be8777e2cf2fd596df
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-14 19:29:37 +0300

    :bug: Fix ErrorBoundary

--- src/components/UI/ErrorBoundary.tsx
+++ src/components/UI/ErrorBoundary.tsx
@@ -8,19 +8,20 @@ interface ErrorBoundaryProps {
 interface ErrorBoundaryState {
   hasError: boolean;
   error: Error | null;
+  errorInfo: ErrorInfo | null;
 }
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
-    this.state = { hasError: false, error: null };
+    this.state = { hasError: false, error: null, errorInfo: null };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can log the error or perform any necessary actions here
     // eslint-disable-next-line no-console
     console.error(error, errorInfo);
-    this.setState({ hasError: true, error });
+    this.setState({ hasError: true, error, errorInfo });
   }
 
   render() {
@@ -28,6 +29,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
       return (
         <div>
           Something went wrong. <p>{this.state.error?.toString()}</p>
+          <p>{this.state.errorInfo?.toString()}</p>
         </div>
       );
     }
