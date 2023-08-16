696822568c51262de0842de9c2d58c6c7100f608
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-16 20:21:27 +0300

    :sparkles: Add LoadingSpinner

---
+++ src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './LoadingSpinner.module.scss';
+
+const LoadingSpinner: FC = () => (
+  <div className={styles.container}>
+    <FontAwesomeIcon icon={faSpinner} spinPulse size="2xl" />
+  </div>
+);
+
+export default LoadingSpinner;
