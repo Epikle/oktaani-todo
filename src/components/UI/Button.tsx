9f97a2e3fe795363097522d07bf8208f8de7b8f3
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-19 14:53:46 +0300

    :recycle: Refactor Button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,15 +1,43 @@
-import { ButtonHTMLAttributes, forwardRef } from 'react';
+import { ButtonHTMLAttributes, forwardRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
+  onClick?: () => void | Promise<void>;
+  toggle?: boolean;
   testId?: string;
 };
 
 const Button = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement> & Props>(
-  ({ testId, children, ...props }, ref) => (
-    <button type="button" ref={ref} data-testid={testId} {...props}>
-      {children}
-    </button>
-  ),
+  ({ testId, toggle: toggleProp = false, onClick, children, ...props }, ref) => {
+    const [loading, setLoading] = useState(false);
+    const [toggle, setToggle] = useState(toggleProp);
+
+    const handler = async () => {
+      if (onClick) {
+        setLoading(true);
+        await onClick();
+        setLoading(false);
+      }
+      if (toggleProp !== undefined) {
+        setToggle((prevS) => !prevS);
+      }
+    };
+
+    return (
+      <button
+        type="button"
+        ref={ref}
+        data-testid={testId}
+        onClick={handler}
+        {...(toggle ? { 'data-active': true } : {})}
+        {...props}
+      >
+        {loading && <FontAwesomeIcon icon={faSpinner} spinPulse />}
+        {!loading && children}
+      </button>
+    );
+  },
 );
 
 Button.displayName = 'Button';
